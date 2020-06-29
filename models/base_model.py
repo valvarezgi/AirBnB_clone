71a317877821f43c824f9cf52a64d5c60ef38098
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-06-29 15:58:00 -0500

    Update classes

---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Module Base"""
+
+
+from uuid import uuid4
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """init - constructor"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(kwargs.get(key),
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "__class__":
+                        pass
+                elif key is not "__class__":
+                        self.__dict__[key] = value
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """str - returns the string representation"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """save - updates the public instance attribute updated_at
+        with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """to_dict -  returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
