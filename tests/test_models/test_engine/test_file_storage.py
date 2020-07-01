2a731e7e38f7691013ea4ee7d4b827d6101f9ed5
Miguel Palacios <oswaldo9605@gmail.com>
2020-07-01 16:40:40 +0000

    Add more test cases
    Co-authored-by: valvarezgi <vale.agiraldo@gmail.com>

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,12 +4,14 @@
 
 import unittest
 import pep8
+import json
 from models import storage
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
+
     """Class to test FileStorage"""
 
     def test_pep8_conformance_FileStorage(self):
@@ -17,6 +19,14 @@ class TestFileStorage(unittest.TestCase):
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/engine/file_storage.py'])
         self.assertEqual(result.total_errors, 0)
+
+    def test_docstrings(self):
+        """Test docstrings"""
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
 
     def setUp(self):
         """Set variables"""
@@ -29,3 +39,30 @@ class TestFileStorage(unittest.TestCase):
         self.assertTrue(hasattr(self.storaged, "new"))
         self.assertTrue(hasattr(self.storaged, "save"))
         self.assertTrue(hasattr(self.storaged, "reload"))
+
+    def test_all_method(self):
+        """Test for all method
+        """
+        dict_tmp = self.storaged.all()
+        self.assertEqual(type(dict_tmp), dict)
+        self.assertIs(dict_tmp, self.storaged._FileStorage__objects)
+
+    def test_new_method(self):
+        """Test for new method
+        """
+        self.storaged.new(self.my_model)
+        key = 'BaseModel.{}'.format(self.my_model.id)
+        self.assertIn(key, self.storaged.all())
+
+    def test_save_method(self):
+        """Test for save method
+        """
+        self.my_model.save()
+        key = "BaseModel.{}".format(self.my_model.id)
+        self.assertIn(key, self.storaged._FileStorage__objects.keys())
+
+    def test_reload_method(self):
+        """Test for reload method
+        """
+        self.storaged.reload()
+        self.assertTrue(len(self.storaged._FileStorage__objects) > 0)
